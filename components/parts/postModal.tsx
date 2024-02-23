d01c4ded458d5403fd9c8b3d762f8720704b20a2
fueri <watanabe@fueri.jp>
2024-02-24 04:40:21 +0900

    update

--- components/parts/postModal.tsx
+++ components/parts/postModal.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Dialog,
   DialogContent,
@@ -10,31 +12,95 @@ import {
 import { Button } from "../ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Textarea } from "../ui/textarea";
+import { getDatabase, set, ref } from "@firebase/database";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useTransition } from "react";
+
+const postSchema = z.object({
+  message: z.string().min(2).max(80),
+});
+
+export type PostSchemaType = z.infer<typeof postSchema>;
 
 const PostModal = () => {
+  const form = useForm<PostSchemaType>({
+    resolver: zodResolver(postSchema),
+  });
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+    reset,
+  } = form;
+  const [isPending, startTransition] = useTransition();
+
+  const sendPost: SubmitHandler<PostSchemaType> = async (data) => {
+    startTransition(async () => {
+      console.log(data);
+      // const db = getDatabase();
+      // const dbRef = ref(db, "post");
+      // set(dbRef, {
+      //   data,
+      // });
+    });
+  };
+
   return (
     <Dialog>
-      <DialogTrigger asChild>
-        <Button>投稿</Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>価値観を共有しましょう</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid w-full gap-2">
-          <Textarea placeholder="例）600円のタバコ" />
-          <p className="text-sm text-muted-foreground">
-            80文字以内で記入してください。
-          </p>
-          <Button>Send message</Button>
-        </div>
-        <DialogFooter></DialogFooter>
-      </DialogContent>
+      <Form {...form}>
+        <form onSubmit={handleSubmit(sendPost)}>
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <DialogTrigger asChild>
+                  <Button>投稿</Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>価値観を共有しましょう</DialogTitle>
+                    <DialogDescription>
+                      This action cannot be undone. This will permanently delete
+                      your account and remove your data from our servers.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid w-full gap-2">
+                    <FormControl>
+                      <Textarea
+                        // id="message"
+                        placeholder="例）600円のタバコ"
+                        // {...register("message", { required: true })}
+                        {...field}
+                      />
+                    </FormControl>
+                    <p className="text-sm text-muted-foreground">
+                      80文字以内で記入してください。
+                    </p>
+                    <Button type="submit" disabled={isPending}>
+                      Send message
+                    </Button>
+                  </div>
+                  <DialogFooter></DialogFooter>
+                </DialogContent>
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
     </Dialog>
   );
 };
---
+++ firebase/client.ts
@@ -0,0 +1,26 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { getFunctions } from "firebase/functions";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCM07p8cMQLAScnyuWw3A1t-eyILsYi0fA",
+  authDomain: "sharevalues-3a252.firebaseapp.com",
+  projectId: "sharevalues-3a252",
+  storageBucket: "sharevalues-3a252.appspot.com",
+  messagingSenderId: "1011933506683",
+  appId: "1:1011933506683:web:cec1c7a4b4ef8a85e986de",
+  measurementId: "G-BPZSY9P2L7",
+};
+
+if (!getApps()?.length) {
+  initializeApp(firebaseConfig);
+}
+
+export const analytics = getAnalytics();
+export const db = getFirestore();
+export const storage = getStorage();
+export const auth = getAuth();
+export const funcions = getFunctions();
