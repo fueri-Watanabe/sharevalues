922c2f6d5984d159fcd4b927e6e6f6468453fede
fueri <watanabe@fueri.jp>
2024-03-07 06:57:13 +0900

    fix

--- utils/convertDate.tsx
+++ utils/convertDate.tsx
@@ -1,9 +1,11 @@
-"use client";
-
 import { format } from "date-fns";
 
 // 記日付の表を（yyyy年MM月dd日）に変換
 export const convertDate_yMd_JP = (value: Date) => {
-  const date = format(value, "yyyy年MM月dd日");
+  const reparseDate = (value: Date) =>
+    value.toLocaleDateString("en-US", {
+      timeZone: "Asia/Tokyo",
+    });
+  const date = format(reparseDate(value), "yyyy年MM月dd日");
   return date;
 };
